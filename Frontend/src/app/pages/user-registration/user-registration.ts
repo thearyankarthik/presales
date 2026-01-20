49c8099149203a25b8e9c1b72def29a880a175c5
thearyankarthik <aryan.karthik2004@gmail.com>
2026-01-20 16:40:25 +0530

    Created user registration page

---
+++ Frontend/src/app/pages/user-registration/user-registration.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registration',
+  imports: [],
+  templateUrl: './user-registration.html',
+  styleUrl: './user-registration.css',
+})
+export class UserRegistration {
+
+}
